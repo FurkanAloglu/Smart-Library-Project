f939d1cebbc7aa5658db5a5fc4189da3e731f36f
Furkan <alogluwork@gmail.com>
2025-12-26 14:58:18 +0300

    ..

--- smart-library-backend/src/main/java/com/furkan/smart_library_backend/SmartLibraryBackendApplication.java
+++ smart-library-backend/src/main/java/com/furkan/smart_library_backend/SmartLibraryBackendApplication.java
@@ -3,23 +3,13 @@ package com.furkan.smart_library_backend;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class SmartLibraryBackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SmartLibraryBackendApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner bcryptPrinter() {
-		return args -> {
-			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
-			String hash = encoder.encode("123");
-			System.out.println("BCrypt hash (123):");
-			System.out.println(hash);
-		};
 	}
 }
